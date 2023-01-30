b4ec43c06f656319cdc18860acb7d0107d1e8007
Alanna <a04810254@gmail.com>
2023-01-30 13:32:12 +0800

    uploadfront

--- About-front/src/pages/IndexPage.vue
+++ About-front/src/pages/IndexPage.vue
@@ -1,17 +1,8 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    早安你好
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
+<script setup>
 </script>
