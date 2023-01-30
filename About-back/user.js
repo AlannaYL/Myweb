26c4e6f165016ffe93b587753958872610bd66cd
Alanna <a04810254@gmail.com>
2023-01-30 08:38:30 +0800

    upload

--- About-back/user.js
+++ About-back/user.js
@@ -5,21 +5,30 @@ const Userschema = new Schema({
   account: {
     type: String,
     required: [true, '缺少帳號'],
-    maxlength: [20],
-    minlenght: [4],
+    maxlength: [20, '帳號太長'],
+    minlenght: [4, '帳號太短'],
     unique: true,
-    match: [/^[a-zA-a0-9]+$/, '帳號只帳號只能是英文數字'],
+    match: [/^[a-zA-Z0-9]+$/, '帳號只能是英文數字'],
     trim: true
+  },
+  passport: {
+    type: String,
+    require: true
   },
   email: {
     type: String,
     required: [true, '缺少信箱'],
     unique: true,
     validator: {
-      validate (value) {
-        return validator.isEmail(value)
+      validate (email) {
+        return validator.isEmail(email)
       },
       message: '信箱格式錯誤'
     }
+  },
+  phone: {
+    type: String,
+    default: [],
+    match: [/^[0-9]{10}$/, '電話只能是數字']
   }
 }, { versionKey: false })
